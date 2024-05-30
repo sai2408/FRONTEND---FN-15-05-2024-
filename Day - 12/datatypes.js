3f30fcc022301686977b73525735c06808ee94d4
Venkata sai vardhan Thimmisetty <57175225+sai2408@users.noreply.github.com>
2024-05-30 11:19:14 +0530

    Add files via upload

---
+++ Day - 12/datatypes.js
@@ -0,0 +1,42 @@
+// Primitive data Types
+var x = 10;
+var y = 20.45;
+var z = "sai vardhan";
+var a = null;
+var b;
+var c = false;
+var d = Symbol("Sai");
+console.log(a);
+console.log(b);
+console.log(c);
+console.log(d);
+console.log(x);
+console.log(y);
+console.log(z);
+
+//Reference Data Types
+var ab = {
+    name : "sai",
+    age : 200
+}
+console.log(ab);
+var ac = [10,20,"sai"];
+console.log(ac);
+function sai(a,b){
+    console.log(a+b);
+    console.log(a-b);
+    console.log(a*b);
+    console.log(a/b);
+}
+sai(10,5);
+sai(15,5);
+
+//Specail Data Types
+var xyz = NaN;
+console.log(xyz);
+abcd = -Infinity
+abcde = Infinity
+console.log(abcd);
+console.log(abcde);
+var m = 1234567123671232356;
+console.log(m);
---
+++ Day - 12/variables.js
@@ -0,0 +1,18 @@
+// Var Varible
+var x = 10;
+x = 20;
+var x = 200;
+console.log(x);
+
+//Let Variable
+let y = 100;
+y = 200;
+let y = 300;
+console.log(y);
+
+//Const Variable
+const z = 100;
+z = 200;
+const z = 300;
+//console.log(z);
+
